2fb9d54e07e342df956418007af49d8c983300c8
gengiti-deenu <deenuy@my.yorku.ca>
2023-01-25 02:38:49 -0500

    added python flask for client requests and model prediction

--- server/app.py
+++ server/app.py
@@ -1,21 +1,30 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, Response, render_template_string
 import pickle
 
 # Create flask app
-flask_app = Flask(__name__)
+# flask_app = Flask(__name__)
+
+flask_app = Flask(__name__, template_folder='templates', static_folder='static')
+ 
+flask_app.config.from_pyfile('config.py')
 model = pickle.load(open("model.pkl", "rb"))
 
-@flask_app.route("/bugzilla_recsys")
-def Home():
-    do something.
+@flask_app.route('/')
+def product():
+  return render_template('index.html')
 
-@flask_app.route("/predict", methods = ["POST"])
+@flask_app.route("/imager_predict", methods = ["POST"])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+  data = request.get_json()
+  
+  print("\n\n")
+  print(data)
+  print("\n\n")
+
+  # prediction = model.predict(features)
+  return Response('Prediction API response is successful', status='HTTP_200_OK')
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+  flask_app.run(debug=True)
+  # app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
---
+++ server/config.py
@@ -0,0 +1,3 @@
+PORT = 5001
+DEBUG = False
+HOST = '0.0.0.0'
